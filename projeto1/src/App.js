a48fe3648ce7e510eb202e7227436b54a8f88eee
Nathalia-Urenha <naty.urenha@gmail.com>
2021-10-05 16:49:48 -0300

    Desafio Número Aleatório

--- projeto1/src/App.js
+++ projeto1/src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 //import Primeiro from './components/Primeiro';
 //import X, {Comp1, Comp2} from './components/Multi';
-import MinMax from './components/MinMax';
-
+//import MinMax from './components/MinMax';
+import Aleatorio from './components/Aleatorio';
 //componente baseado em função
 export default () => (
   <View style={style.App}>
-    <MinMax min={3} max="20" />
+    <Aleatorio min={1} max={60} />
+    <Aleatorio min={1} max={60} />
+    <Aleatorio min={1} max={60} />
+    <Aleatorio min={1} max={60} />
+    <Aleatorio min={1} max={60} />
+    <Aleatorio min={1} max={60} />
+    <Aleatorio min={1} max={60} />
+    {/*<MinMax min={3} max="20" />
     <MinMax min="1" max="94" />
-    {/*<X />
+    <X />
     <Comp1 />
     <Comp2 />
     <Primeiro /> */}
---
+++ projeto1/src/components/Aleatorio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from  'react-native'
+import Estilo from './estilo'
+
+export default props => {
+  const {min, max} = props
+  const delta = max - min + 1
+  const aleatorio= parseInt(Math.random() * delta) + min;
+  return(
+    <Text style={Estilo.txtG}>
+      O valor aleatório é: {aleatorio}
+    </Text>
+  )
+}
